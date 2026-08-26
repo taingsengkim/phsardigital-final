be153a31e13da3d35f8622424d831f509eee240b
Taing Sengkim <taing.sengkim3110@gmail.com>
2026-08-26 12:54:13 +0700

    Add Google Fonts preconnect and Hanuman stylesheet link to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,11 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${inter.variable} ${playfair.variable}`}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Hanuman:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`${inter.className} ${playfair.variable} min-h-full flex flex-col text-base antialiased`}
       >
